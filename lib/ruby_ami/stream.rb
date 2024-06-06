6c2c025bc35f84806a66722e7ede33e292a02ee6
Thomas <{ID}+{username}@users.noreply.github.com>
2024-06-06 09:13:54 +0200

    stream : small cleanup

--- lib/ruby_ami/stream.rb
+++ lib/ruby_ami/stream.rb
@@ -36,10 +36,15 @@ module RubyAMI
       define_method("#{state}?") { @state == state }
     end
 
+    # Must be called in EM main thread.
+    # This can be done by wrapping with EM.next_tick
     def async_send_action(*args, &block)
       action = Action.new *args, &block
       # puts "[SEND] #{action.to_s}"
-      register_sent_action action
+      @sent_actions[action.action_id] = action
+      if action.has_causal_events?
+        @causal_actions[action.action_id] = action
+      end
       send_data action.to_s
       action
     end
@@ -113,15 +118,15 @@ module RubyAMI
       # puts "[RECV] #{message.inspect}"
       case message
       when Event
-        action = causal_action_for_event message
+        action = @causal_actions[message.action_id]
         if action
           action << message
-          complete_causal_action_for_event message if action.complete?
+          @causal_actions.delete(message.action_id) if action.complete?
         else
           fire_event message
         end
       when Response, Error
-        action = sent_action_for_response message
+        action = @sent_actions.delete(message.action_id)
         raise "Received an AMI response with an unrecognized ActionID! #{message.inspect}" unless action
         action << message
       end
@@ -144,31 +149,6 @@ module RubyAMI
 
     def fire_event(event)
       @event_callback.call event
-    end
-
-    def register_sent_action(action)
-      @sent_actions[action.action_id] = action
-      register_causal_action action if action.has_causal_events?
-    end
-
-    def sent_action_with_id(action_id)
-      @sent_actions.delete action_id
-    end
-
-    def sent_action_for_response(response)
-      sent_action_with_id response.action_id
-    end
-
-    def register_causal_action(action)
-      @causal_actions[action.action_id] = action
-    end
-
-    def causal_action_for_event(event)
-      @causal_actions[event.action_id]
-    end
-
-    def complete_causal_action_for_event(event)
-      @causal_actions.delete event.action_id
     end
   end
 end
